2615c65dc7773156ae33ec53eecafc7836027854
leedahye <leedahye2001@naver.com>
2023-06-19 01:17:56 +0900

    feat: 테스트 button 컴포넌트 생성

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,18 @@
-interface Props {}
+type Props = {
+  text: string;
+  size?: "small" | "big";
+};
 
-const Button = (props: Props) => {
-  return <div>Button</div>;
+const Button = ({ text, size = "small" }: Props) => {
+  return (
+    <button
+      className={`bg-white rounded-sm text-base text-black hover:opacity-90 transition-opacity shadow-lg shadow-[#5580FF]
+          ${size === "big" ? "p-4 text-2xl" : "p-[0.3rem] text-base"}
+        `}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
