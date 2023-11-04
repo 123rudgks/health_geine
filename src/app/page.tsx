65088e96749210d723962e07876a0bd15641f4e7
KimGyeongHan <123rudgks@gmail.com>
2023-11-04 22:36:13 +0900

    fix : push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,34 @@
 'use client';
 
 import BottomNavigationBar from '@/components/BottomNavigationBar/BottomNavigationBar';
+import { setCookie } from 'cookies-next';
 import Link from 'next/link';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 export default function Home() {
   const [inputValue, setInputValue] = useState<string>('');
+  const [isTrainerMode, setIsTrainerMode] = useState<boolean>(false);
+  useEffect(() => {
+    isTrainerMode
+      ? setCookie('isTrainerMode', 'true')
+      : setCookie('isTrainerMode', 'false');
+  }, [isTrainerMode]);
+
   return (
     <main>
       <div className="flex flex-col">
+        <div
+          className={twMerge(
+            'relative flex h-4 w-8 items-center rounded-full  ring-1 ring-gray-500',
+            isTrainerMode ? 'flex-row-reverse bg-black' : ' bg-gray-500'
+          )}
+          onClick={() => setIsTrainerMode((prev) => !prev)}
+        >
+          <div className="top-0 h-3 w-3  rounded-full  bg-white " />
+        </div>
+        <div>{isTrainerMode ? '트레이너' : '유저'}</div>
         <Link href={'/my-page'}>마이페이지</Link>
         <Link href={'/pop-up'}>팝업</Link>
         <Link href={'/write-health-state'}>건강상태작성</Link>
