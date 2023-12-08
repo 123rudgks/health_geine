a01a10aac883ff1c632a1ae33ff9e01117191511
leedahye <leedahye2001@naver.com>
2023-12-08 21:35:29 +0900

    feat: screen size 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,22 @@ module.exports = {
       fontFamily: FONT_FAMILY,
       colors: COLORS,
     },
+    screens: {
+      sm: '200px',
+      // => @media (min-width: 200px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [],
 };
