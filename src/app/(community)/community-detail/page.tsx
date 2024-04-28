144ac17b6e5672f364929c9d399461a829bcefc8
leedahye <leedahye2001@naver.com>
2024-04-29 00:45:06 +0900

    feat: 커뮤니티 글 작성페이지 UI 개발

--- src/app/(community)/community-detail/page.tsx
+++ src/app/(community)/community-detail/page.tsx
@@ -4,9 +4,14 @@ import TopBottomBarTemplate from '@/components/Template/TopBottomBarPage';
 import BackSpaceArrow from '@/svgs/BackSpaceArrow.svg';
 import CommunityCalendar from '@/svgs/CommunituCalendar.svg';
 import CommunityClock from '@/svgs/CommunityClock.svg';
-import Box from '@/components/Box/Box';
+import Chatting from '@/svgs/FillChatting.svg';
+import EmptyLike from '@/svgs/EmptyLike.svg';
+import SettingDot from '@/svgs/SettingDot.svg';
+import BlackDot from '@/svgs/BlackDot.svg';
 import CommunityListItem from '@/components/pages/community/CommunityListItem';
 import { useRouter } from 'next/navigation';
+import BasicInput from '@/components/Input/BasicInput';
+import ChatSend from '@/svgs/ChatSend.svg';
 
 type Props = {};
 
@@ -29,45 +34,54 @@ const Page = (props: Props) => {
           </div>
         </div>
       }
-      _bottomNode={<BottomNavigationBar />}
+      _bottomNode={
+        <div className="flex h-full items-center border-t-2 bg-transparent px-7 ">
+          <div className="flex flex-1 items-center gap-3">
+            <div className="h-[27px] w-[27px] rounded-full bg-black" />
+            <BasicInput
+              _wrapperProps={{
+                className:
+                  'bg-white ring-[#D4D4D4] px-4 rounded-[18px] h-9 flex-1',
+              }}
+              _inputProps={{
+                className: 'text-[13px] font-noto placeholder:text-[#A6A6A6]',
+                placeholder: '댓글을 입력해주세요.',
+              }}
+              _value={''}
+              _onChange={() => {}}
+            />
+            <ChatSend />
+          </div>
+        </div>
+      }
       _contentDivProps={{ className: 'bg-white' }}
     >
-      <div className="flex flex-col items-center justify-center p-10">
-        {/* <Box className="my-6 flex w-[328px] justify-between rounded-[6px] bg-primary-400 p-3 font-noto">
-          <div className="flex items-center gap-4">
-            <HotSpring />
-            <h1 className="text-[13px] font-bold text-white">
-              한 달 만에 10kg 뺀 썰 푼다.
-            </h1>
-          </div>
-          <div className="flex items-end gap-1">
-            <Like />
-            <span className="font-regular font-noto text-[7px] text-[#F44B4B]">
-              37
-            </span>
-          </div>
-        </Box> */}
-        <div className="flex items-center justify-center gap-1">
+      <div className="flex flex-col items-start gap-2 px-14">
+        <div className="flex items-center gap-1">
           <div className="h-[41px] w-[41px] rounded-full bg-black" />
           <h1 className="font-noto text-[20px] font-semibold text-black">
             정수영
           </h1>
         </div>
-        <h1 className="font-noto text-[18px] font-semibold text-black">
-          다이어트 방법 공유 ㅠㅠ
-        </h1>
-        <div className="font-regular flex gap-2 font-noto text-[13.51px] text-[#434343]">
-          <div className="flex items-center justify-center gap-1">
-            <CommunityCalendar />
-            <p>2023.09.11(월)</p>
-          </div>
-          <div className="flex items-center justify-center gap-1">
-            <CommunityClock />
-            <p>20:34</p>
+        <div className="gap-1">
+          <h1 className="font-noto text-[18px] font-semibold text-black">
+            다이어트 방법 공유 ㅠㅠ
+          </h1>
+          <div className="font-regular flex gap-2 font-noto text-[13.51px] text-[#434343]">
+            <div className="flex items-center justify-center gap-1">
+              <CommunityCalendar />
+              <p>2023.09.11(월)</p>
+            </div>
+            <div className="flex items-center justify-center gap-1">
+              <CommunityClock />
+              <p>20:34</p>
+            </div>
           </div>
         </div>
-
-        <div className="h-[258px] w-[328px] rounded-xl bg-black" />
+        <div className="h-[258px] w-full rounded-xl bg-black" />
+        <div className="flex w-full justify-center pb-2 pt-1">
+          <BlackDot />
+        </div>
         <p className="font-regular font-noto text-[13px]">
           제대로 다이어트 좀 시작해보고 싶은데 방법을 몰라서 시작도 못하고
           있다.. 수분 빼는 다이어트든.. 아무렴 상관없으니까 아무나 나 좀
@@ -75,7 +89,43 @@ const Page = (props: Props) => {
           해볼까 하는데 추천해 줄 방법이나 유튜브 영상같은 거 있으면 댓글로
           공유해주면 참고해서 볼게 ㅠ
         </p>
-        <hr className="h-[0.5px] bg-[#c1c1c1]" />
+        <div className="h-2 w-full border-b border-[#c1c1c1]" />
+        <div className="flex gap-2">
+          <div className="flex gap-1">
+            <Chatting width={21} />
+            <p className="font-regular font-noto text-[13.36px] text-primary-400">
+              12
+            </p>
+          </div>
+          <div className="flex gap-1">
+            <EmptyLike />
+            <p className="font-regular font-noto text-[13.36px] text-[#F44B4B]">
+              13
+            </p>
+          </div>
+        </div>
+        <div className="flex w-full items-center justify-between">
+          <div className="flex items-start justify-center gap-2">
+            <div className="h-[28.99px] w-[28.99px] rounded-full bg-black" />
+            <div>
+              <h1 className="font-noto text-[14.49px] font-semibold text-black">
+                이다혜
+              </h1>
+              <p className="font-regular font-noto text-[14.52px] text-black">
+                저도 딱 그 상황이라 공감가네요...ㅠㅠ
+              </p>
+              <div className="flex gap-4">
+                <p className="font-regular font-noto text-[11.17px] text-[#c1c1c1]">
+                  방금 전
+                </p>
+                <p className="font-regular font-noto text-[11.17px] text-black">
+                  답글쓰기
+                </p>
+              </div>
+            </div>
+          </div>
+          <SettingDot />
+        </div>
       </div>
     </TopBottomBarTemplate>
   );
---
+++ src/app/(community)/write-community/page.tsx
@@ -0,0 +1,100 @@
+'use client';
+import BottomNavigationBar from '@/components/BottomNavigationBar/BottomNavigationBar';
+import TopBottomBarTemplate from '@/components/Template/TopBottomBarPage';
+import BackSpaceArrow from '@/svgs/BackSpaceArrow.svg';
+import CommunityCalendar from '@/svgs/CommunituCalendar.svg';
+import CommunityClock from '@/svgs/CommunityClock.svg';
+import Chatting from '@/svgs/FillChatting.svg';
+import EmptyLike from '@/svgs/EmptyLike.svg';
+import SettingDot from '@/svgs/SettingDot.svg';
+import BlackDot from '@/svgs/BlackDot.svg';
+import CommunityListItem from '@/components/pages/community/CommunityListItem';
+import { useRouter } from 'next/navigation';
+import BasicInput from '@/components/Input/BasicInput';
+import Mask from '@/svgs/InputDelete.svg';
+import Button from '@/components/Button/Button';
+import Box from '@/components/Box/Box';
+import Camera from '@/svgs/ChatCamera.svg';
+
+type Props = {};
+
+const Page = (props: Props) => {
+  const router = useRouter();
+
+  return (
+    <TopBottomBarTemplate
+      _topNode={
+        <div className="relative flex h-full w-full items-center">
+          <div className="absolute left-[22px]">
+            <BackSpaceArrow
+              onClick={() => {
+                router.back();
+              }}
+            />
+          </div>
+          <div className="flex flex-1 justify-center font-noto text-[18px] font-bold text-black">
+            글 작성하기
+          </div>
+        </div>
+      }
+      _bottomNode={
+        <div className="flex justify-center px-14">
+          <Button
+            ring="none"
+            background="primary-400"
+            color="white"
+            className="h-[50px] w-full rounded-[6px] text-[16px] font-semibold"
+          >
+            완료
+          </Button>
+        </div>
+      }
+      _contentDivProps={{ className: 'bg-white' }}
+    >
+      <div className="flex flex-col items-start gap-2 px-14">
+        <div className="gap-1">
+          <div className="font-regular flex gap-2 font-noto text-[18.36px] text-[#434343]">
+            <div className="flex items-center justify-center gap-1">
+              <CommunityCalendar />
+              <p>2023.09.11(월)</p>
+            </div>
+          </div>
+        </div>
+        <BasicInput
+          _wrapperProps={{
+            className:
+              'bg-white ring-[#c1c1c1] px-4 rounded-[6px] h-[41px] flex-1 w-full',
+          }}
+          _inputProps={{
+            className:
+              'text-[16px] font-noto placeholder:text-black text-black',
+            placeholder: '제목을 입력해주세요. (20자 이내)',
+          }}
+          _value={''}
+          _onChange={() => {}}
+        />
+        <Box className="flex w-full flex-col items-center justify-center rounded-[6px] border border-[#c1c1c1] p-0">
+          <textarea
+            className="m-2 h-[200px] w-full resize-none p-2 font-noto text-[16px] text-black placeholder:text-black"
+            placeholder="내용을 입력해주세요."
+          />
+          <div className="h-0 w-full border-b border-[#c1c1c1]" />
+          <div className="flex items-center justify-center p-4 font-noto text-[14px] font-semibold">
+            <Camera />
+            <p className="pl-2 ">사진 선택하기</p>
+            <p>3/5</p>
+          </div>
+        </Box>
+        <div className="grid w-full grid-cols-3 gap-4 pt-2">
+          <div
+            className="flex aspect-square justify-end overflow-hidden rounded-2xl bg-black p-2"
+            style={{ position: 'relative' }}
+          >
+            <Mask />
+          </div>
+        </div>
+      </div>
+    </TopBottomBarTemplate>
+  );
+};
+export default Page;
